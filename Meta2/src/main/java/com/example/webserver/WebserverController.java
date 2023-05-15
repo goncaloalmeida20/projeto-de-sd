6270c2c021c32181e7f8c132b8d659142d22f933
Gonçalo Almeida <gamertota4@gmail.com>
2023-05-15 10:50:54 +0100

    html (Part 1)

--- Meta2/src/main/java/com/example/webserver/WebserverController.java
+++ Meta2/src/main/java/com/example/webserver/WebserverController.java
@@ -4,6 +4,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+import RMISearchModule.SearchModuleC_S_I;
+import classes.Page;
 
 @Controller
 public class WebserverController {
@@ -51,5 +53,69 @@ public class WebserverController {
     @GetMapping("/client")
     public String clientPage() {
         return "guest";
+    }
+
+    @GetMapping("/index-url")
+    public String indexUrlForm() {
+        return "index-url";
+    }
+
+    @PostMapping("/index-url")
+    public String indexUrl(@RequestParam("urlInput") String url) {
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 6000);
+            SearchModuleC searchC = (SearchModuleC) registry.lookup("searchC");
+
+            searchC.indexUrl(url.toLowerCase());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //TODO: Display a message or perform any necessary actions after successful indexation
+
+
+        return "redirect:/index-url";
+    }
+
+    @PostMapping("/process-form")
+    public String processForm(@RequestParam("numTerms") int numTerms, @RequestParam("term") String[] terms) {
+        // Process the terms
+
+        // TESTE
+        for (int i = 0; i < numTerms; i++) {
+            System.out.println("Term " + (i + 1) + ": " + terms[i]);
+        }
+
+        return "redirect:/index";
+    }
+
+    @PostMapping("/search")
+    public String search(@RequestParam("url") String url, Model model) {
+        List<Page> pages = searchResults(url);
+        model.addAttribute("pages", pages);
+
+        return "search-results";
+    }
+
+    private List<Page> searchResults(String url) {
+
+        //TESTE
+
+        List<Page> pages = new ArrayList<>();
+
+        Page page1 = new Page();
+        page1.url = "https://example.com/page1";
+        page1.title = "Page 1";
+        page1.citation = "Citation 1";
+        pages.add(page1);
+
+        Page page2 = new Page();
+        page2.url = "https://example.com/page2";
+        page2.title = "Page 2";
+        page2.citation = "Citation 2";
+        pages.add(page2);
+
+        return pages;
     }
 }
